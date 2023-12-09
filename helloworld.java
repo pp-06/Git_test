bf8b3cca2911e91964c37ef3798d870b6c4c3899
pp-06 <parth.parmar@nineleaps.com>
2023-12-09 14:30:26 +0530

    hello world latest commit

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,8 @@
 class HelloWorld{
     public static void main(String[] args){
-        System.out.println("Hello, World!");
+        int a = 50;
+        int b = 10;
+        int c = a + b;
+        System.out.println(c);
     }
 }
